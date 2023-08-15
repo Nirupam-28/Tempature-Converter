717393f0e61eae64e2c02167e19c5cefc4ede6be
Nirupam-28 <114160942+Nirupam-28@users.noreply.github.com>
2023-08-15 12:49:09 +0530

    script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ cel.oninput=function(){
 kel.oninput=function(){
    let fa=(parseFloat(kel.value)-273.15)*9/5+32;
     fah.value=parseFloat(fa.toFixed(2));
-    let ce=(parseFloat(ke.value)-273.15);
+    let ce=(parseFloat(kel.value)-273.15);
     cel.value=parseFloat(ce.toFixed(2));
 }
 fah.oninput=function(){
     let ke=(parseFloat(fah.value)-32)*5/9+273.15;
     kel.value=parseFloat(ke.toFixed(2));
-    let ce=(parseFloat(fah.value)-32*5)/9;
+    let ce=(parseFloat(fah.value)-32)*5/9;
     cel.value=parseFloat(ce.toFixed(2));
 
 }
